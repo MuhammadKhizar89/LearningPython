e0b5936bc146e1099bbec7b96c3ff4c65297129d
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-22 09:33:48 +0500

    First Calculator

--- Learning.py
+++ Learning.py
@@ -1,1 +1,16 @@
 print("Hi");
+a=complex(8,2)
+print (a)
+#mutable ->Changes---->List
+
+#immutable ->Doesn't--->tuple
+
+# dictionary->key and value (objects in javascript)
+
+# **->power 2**4 =16  2power4=16
+# //->floor division 8//5=1
+#Simple Calculator
+# by default it takes string
+num1=input("Enter Number:");
+num2=input("Enter Number:");
+print("Addition",int(num1)+int(num2));
