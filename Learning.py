853642a7c7d8e37d1c00e644957aecac6d66a28f
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-22 11:09:40 +0500

    More Code

--- Learning.py
+++ Learning.py
@@ -9,8 +9,23 @@ print (a)
 
 # **->power 2**4 =16  2power4=16
 # //->floor division 8//5=1
+
 #Simple Calculator
 # by default it takes string
-num1=input("Enter Number:");
-num2=input("Enter Number:");
-print("Addition",int(num1)+int(num2));
+
+# num1=input("Enter Number:");
+# num2=input("Enter Number:");
+# print("Addition",int(num1)+int(num2));
+
+# num1=int(input("Enter Number:"));
+# num2=int(input("Enter Number:"));
+# print("Addition",num1+num2);
+
+abc="hello" 
+new=abc[0:2] # 2 index sy peechy peechy
+print (new)
+
+srtipp="!!!hellom!1!2!!"
+print(srtipp.lstrip("!"))
+print(srtipp.rstrip("!"))
+print(srtipp.strip("!"))
