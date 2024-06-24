a5e73c7790984ba5e912c08c3118b994129d6015
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-24 22:27:19 +0500

    IO Seek and Tell

--- Learning.py
+++ Learning.py
@@ -79,74 +79,119 @@
 # print(k);
 # print(list)
 
-# Tuple
-tup=(1,2,"hehe")
-print(tup);
-if 2 in tup:
-    print("Present")
+# # Tuple
+# tup=(1,2,"hehe")
+# print(tup);
+# if 2 in tup:
+#     print("Present")
 
-# slicing krny kay baad new tuple return hoga previous change ni hoga
-tup2=tup[0:2];
-print(tup2);
-# We can cancatinate two tuples
-print(tup.count(3));
-print(tup.index(1,0,2)); #tofind,starting index,ending index
-# to change tuple convert it into list
-# Define a tuple
-GFG_tuple = (1, 2, 3)
+# # slicing krny kay baad new tuple return hoga previous change ni hoga
+# tup2=tup[0:2];
+# print(tup2);
+# # We can cancatinate two tuples
+# print(tup.count(3));
+# print(tup.index(1,0,2)); #tofind,starting index,ending index
+# # to change tuple convert it into list
+# # Define a tuple
+# GFG_tuple = (1, 2, 3)
  
-# Convert the tuple to a list for changing
-GFG_list = list(GFG_tuple)
-GFG_list.append(2);
-print(GFG_list)
-GFG_tuple=tuple(GFG_list);
-print(GFG_tuple)
+# # Convert the tuple to a list for changing
+# GFG_list = list(GFG_tuple)
+# GFG_list.append(2);
+# print(GFG_list)
+# GFG_tuple=tuple(GFG_list);
+# print(GFG_tuple)
 
-# fString
-a="hello"
-b="world"
-print(f"({a} hehe {b})");
-# or old tradition (a.format(b))
-letter="My Name is{} and I am {} years old"
-age="18"
-name="abc"
-print(letter.format(name,age));
-# format can do more things like floating will be upto 2 decimal
+# # fString
+# a="hello"
+# b="world"
+# print(f"({a} hehe {b})");
+# # or old tradition (a.format(b))
+# letter="My Name is{} and I am {} years old"
+# age="18"
+# name="abc"
+# print(letter.format(name,age));
+# # format can do more things like floating will be upto 2 decimal
 
-# DocString
-def square(n):
-    '''Return the square of a number'''
-    return n**2
-print(square(5));
+# # DocString
+# def square(n):
+#     '''Return the square of a number'''
+#     return n**2
+# print(square(5));
 
-print(square.__doc__)#It returns the docstring means comment of the function
-# To document our code Only work after defination of function
+# print(square.__doc__)#It returns the docstring means comment of the function
+# # To document our code Only work after defination of function
 
-#Recursion
-def fab(n):
-    if(n==0):
-        return 0;
-    if(n==1):
-        return 1;
-    return fab(n-1)+fab(n-2);
+# #Recursion
+# def fab(n):
+#     if(n==0):
+#         return 0;
+#     if(n==1):
+#         return 1;
+#     return fab(n-1)+fab(n-2);
 
-for a in range(6):
- print(fab(a));
+# for a in range(6):
+#  print(fab(a));
 
-#  Sets
-s={1,2,3,3,"hello","hello"}
-print(s);
-# No gaurantee of order
-ab=set(); #to make empty set if we do this{} then first it thinks it is dictionary
-print(type(s),type(ab))
+# #  Sets
+# s={1,2,3,3,"hello","hello"}
+# print(s);
+# # No gaurantee of order
+# ab=set(); #to make empty set if we do this{} then first it thinks it is dictionary
+# print(type(s),type(ab))
 
-for value in s:
-    print(value);
-s2={"hello","World"}
-print(s.union(s2));
-print(s);
-s.update(s2);
-print(s);
-s.intersection_update(s2); #update will the orignal one
-print(s)
-s.symmetric_difference(s2);# for values that are not comn in each others
+# for value in s:
+#     print(value);
+# s2={"hello","World"}
+# print(s.union(s2));
+# print(s);
+# s.update(s2);
+# print(s);
+# s.intersection_update(s2); #update will the orignal one
+# print(s);
+# s.symmetric_difference(s2);# for values that are not comn in each others
+# s.difference(s2); # A-B vala kaam hy
+# s2.isdisjoint(s);
+# s2.issubset(s);
+# s2.issuperset(s);
+# s.add("hehe");
+# print(s);
+# s.remove("hehe");
+# print(s)
+# s.discard("hehe"); #remove shows error if that elemet not present but discard not
+# print(s)
+# a=s.pop();
+# print(a)
+# del s; #delete full set
+
+# # dictionaries
+# dic={
+#     "name":"hello",
+#     "class":5,
+#     "age":18,
+#     8:90
+#     }
+# print(dic["name"]);
+# print(dic);
+# print(dic.get('hehe')); #it will not  through error if key is not present but simple will through error dic['hehe']
+# print(dic.keys());
+# print(dic.values());
+# print(dic.items());
+# dic2={3:5};
+# dic.update(dic2);
+# print(dic);
+# dic.pop(8);
+# dic.popitem(); #last iem will be deleted
+# dic.clear();
+# print(dic);
+
+# #for loop and else
+# for i in range(10):
+#     print(i)        
+# else:
+#     print("done")
+
+# # Agr break lga dain loop may tou else ni print hoga
+
+### Seek and tell are like pointers in file that tell moves pointer to start from reading
+
