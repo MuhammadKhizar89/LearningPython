6fa8223f9ca7e7bdf9cc003b3db552439617eab4
unknown <muhammadkhizar8919@gmail.com>
2025-08-31 21:31:00 +0500

    Basic

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 print("hello world")
+a="my name is khan"
+print(a[0:8:2])  #Start:End:Step
+print(a[0:8:2].capitalize()) #capitalize
+print(type(a))
+b=int(3.6)
+print(type(b))
+
+
+
+a=29
+b=3
+print(a/b)
+print(a//b) #Points kay baad vali values ignore
+print(a**b) #power
+
+
+for i in range(1,10,1): #start end (n-1 tk chly ga ) step
+    print(i)
+
+print(len("haha")) #js may length cheez ka func us oobject ka hota tha but in python hr func globaly available hota h
+
+for i in range(10):     #0 sy n-1 tk chly ga
+    print(i)
+
+# Lists    # 
+a=[1,2,3,4,5,True,print()]
+print(a[0])
+print(a[0:4])
+print(a[-2])
